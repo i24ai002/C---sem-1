d002e5b7f9220faba83826610a8d053568cf4252
i24ai002 <i24ai002@aid.svnit.ac.in>
2024-11-20 20:03:27 +0530

    Add files via upload

---
+++ Lab 5 & Lab 6/program 69.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{   
+    int arr[10],f,flag=0,i=0;
+    printf("enter array of number\n ");
+    for(int i=0;i<10;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("enter element you want to search\n ");
+    scanf("%d",&f);
+    for(i;i<10;i++)
+    {
+        if(f==arr[i])
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1)
+    {
+        printf("\n %d found at index %d ",f,i);
+    }
+    else
+    printf("element not found");
+    return 0;
+}
